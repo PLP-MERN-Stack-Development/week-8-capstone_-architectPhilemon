76b1fd8a79d1e1665323c6e036cc92e63481c44a
Philemon mwendwa maithya <mwendwaphilemon84@gmail.com>
2025-07-18 23:33:45 +0300

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -19,7 +19,8 @@ export default async function HomePage() {
   let error: string | null = null
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
+    const res = await fetch('https://week-8-capstone-architectphilemon.onrender.com/api/events')  // ✅ Or via `process.env`
+
 
 
     if (!res.ok) {
